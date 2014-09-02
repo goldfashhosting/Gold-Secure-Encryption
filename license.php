b0e0ad13067686c1205656bf45f04c343979c5ea
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-09-02 15:56:48 -0400

    Create license.php

---
+++ license.php
@@ -0,0 +1,8 @@
+<?php
+// IF YOU DO NOT KNOW THE CREDENTIALS FOR BELOW PLEASE CALL GOLDFASH HOSTING 302 - 387 - 4653
+//Enter License Key Below.
+$goldECSecurity="";
+//Enter Security Auth Code
+$hashECAuth="";
+require('ee_con/0..0.0.php');
+?>
