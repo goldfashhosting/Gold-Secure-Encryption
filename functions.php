60623530720f5d11669befce4aaa98bcd7ff55ec
goldfashhosting <goldfashhosting@users.noreply.github.com>
2014-09-03 14:40:48 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function my_github_plugin_updater() {
 	github_plugin_updater_register( array(
 		'owner'	=> 'goldfashhosting',
 		'repo'	=> 'GoldFash-Secure-Encryption',
-		'slug'	=> 'GoldFash-Secure-Encryption/goldsecure.php', // defaults to the repo value ('repo/repo.php')
+		'slug'	=> 'GoldFash-Secure-Encryption/goldsecure.php', 
 	) );
-}
+
 
